990e977d2557ee45ce74483fcb2235475910809f
Simon Adcock <simonadcock2@gmail.com>
2020-08-26 18:03:03 +0100

    reorganise stories under checkbox and checkbox group titles

--- src/core/components/checkbox/checkbox-group.stories.tsx
+++ src/core/components/checkbox/checkbox-group.stories.tsx
@@ -1,15 +1,13 @@
+import { CheckboxGroup } from "./index"
+
 export default {
-	title: "Checkbox",
+	title: "Checkbox Group",
+	component: CheckboxGroup,
 }
 export * from "./stories/controlled"
 export * from "./stories/default"
 export * from "./stories/error"
-export * from "./stories/indeterminate"
 export * from "./stories/legend"
 export * from "./stories/legend-error"
 export * from "./stories/legend-supporting-text"
 export * from "./stories/select-all"
-export * from "./stories/supporting-text"
-export * from "./stories/supporting-text-only"
-export * from "./stories/ungrouped"
-export * from "./stories/unlabelled"
---
+++ src/core/components/checkbox/checkbox.stories.tsx
@@ -0,0 +1,11 @@
+import { Checkbox } from "./index"
+
+export default {
+	title: "Checkbox",
+	component: Checkbox,
+}
+export * from "./stories/indeterminate"
+export * from "./stories/supporting-text"
+export * from "./stories/supporting-text-only"
+export * from "./stories/ungrouped"
+export * from "./stories/unlabelled"
